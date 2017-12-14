2d0b1e18886838304ed18103e45406d82e2f2aea
MrsPepa <jose.leon1996@gmail.com>
2017-12-14 11:10:49 -0300

    actualizando vista 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,69 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".logo").fadeOut(1500);
+  },3000);
+  $('.first').hide();
+});
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".first").fadeIn(1500);
+  },3000);
+});
+
 $(document).ready(function(){
   $('.second').hide();
+  $('.third').hide();
   $('#btn2').click(function(){
     $('.second').show();
     $('.first').hide();
+    
   })
 
-  $('.iconPrev').click(function(){
-    $('.first').show();
-    $('.second').hide();
+})
+/* seleccion hacia atras*/
+$('.iconPrev').click(function(){
+  $('.first').show();
+  $('.second').hide();
+  $('.third').hide();
   })
-  /*habilitar boton*/
-  $('#fono').keyup(function(){
-    $('#next').addClass('btn-next');
-  })
-  /*
-  })*/
+$('.iconPrev1').click(function(){
+  $('.first').hide();
+  $('.second').show();
+  $('.third').hide();
+})
+
+/*habilitar boton*/
+$('#fono').keyup(function(){
+  if($(this).val().length < 10){
+    $('#next').attr('class','disabled');
+    $('#next').attr('disabled','disabled');
+  } else {
+    $('#next').removeAttr('class','disabled');
+    $('#next').removeAttr('disabled','false');
+    $('#next').attr('id','btn-next');
+  }
+  /*comprobar que sólo se ingresen numeros*/
+  if(isNaN($('#fono').val())){
+    alert('Favor ingresar sólo números');
+    $('#next').attr('class','disabled');
+    return false;
+  }
+
+  return false;
+})
+$('#fono').keyup(function(){
+  if($(this).val().length===10){
+    var code = '';
+    var num = '123456789';
+    for(var i = 0; i < 3; i++){
+    code+=num.charAt(Math.floor(Math.random()*num.length));
+    }
+    alert('Tu código LAB: '+code);
+  }
+})
+/* ingresar codigo */
+$('#next').click(function(){
+  $('.first').hide();
+  $('.second').hide();
+  $('.third').show();
 })
